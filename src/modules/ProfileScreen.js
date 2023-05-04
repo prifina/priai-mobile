4a7482372b56a53d95b151803865fd5ad3089887
hamza <hamzahalilovic.12@gmail.com>
2023-05-04 13:36:54 +0200

    testflight 0.0.2

--- src/modules/ProfileScreen.js
+++ src/modules/ProfileScreen.js
@@ -63,45 +63,45 @@ const ProfileScreen = ({navigation}) => {
   /////APPLE HEALTHKIT
 
   /* Permission options */
-  // const permissions = {
-  //   permissions: {
-  //     read: [
-  //       AppleHealthKit.Constants.Permissions.StepCount,
-  //       AppleHealthKit.Constants.Permissions.Steps,
-  //     ],
-  //     write: [AppleHealthKit.Constants.Permissions.Steps],
-  //   },
-  // };
+  const permissions = {
+    permissions: {
+      read: [
+        AppleHealthKit.Constants.Permissions.StepCount,
+        AppleHealthKit.Constants.Permissions.Steps,
+      ],
+      write: [AppleHealthKit.Constants.Permissions.Steps],
+    },
+  };
 
-  // const options = {
-  //   startDate: new Date(2020, 1, 1).toISOString(),
-  // };
+  const options = {
+    startDate: new Date(2020, 1, 1).toISOString(),
+  };
 
-  // AppleHealthKit.initHealthKit(permissions, error => {
-  //   /* Called after we receive a response from the system */
+  AppleHealthKit.initHealthKit(permissions, error => {
+    /* Called after we receive a response from the system */
 
-  //   if (error) {
-  //     console.log('[ERROR] Cannot grant permissions!');
-  //   }
+    if (error) {
+      console.log('[ERROR] Cannot grant permissions!');
+    }
 
-  //   /* Can now read or write to HealthKit */
+    /* Can now read or write to HealthKit */
 
-  //   AppleHealthKit.getHeartRateSamples(options, (callbackError, results) => {
-  //     /* Samples are now collected from HealthKit */
-  //   });
-  // });
+    AppleHealthKit.getHeartRateSamples(options, (callbackError, results) => {
+      /* Samples are now collected from HealthKit */
+    });
+  });
 
-  // const [stepCount, setStepCount] = useState([]);
+  const [stepCount, setStepCount] = useState([]);
 
-  // AppleHealthKit.getStepCount(options, (err, results) => {
-  //   if (err) {
-  //     console.log('error results');
+  AppleHealthKit.getStepCount(options, (err, results) => {
+    if (err) {
+      console.log('error results');
 
-  //     return;
-  //   }
-  //   console.log('results', results);
-  //   setStepCount(results.value);
-  // });
+      return;
+    }
+    console.log('results', results);
+    setStepCount(results.value);
+  });
 
   const deviceHeight = useHeaderHeight();
 
