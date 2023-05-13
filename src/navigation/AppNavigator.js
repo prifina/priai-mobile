30608a854f3bfbac6ba01aaef1f61a2490d130b4
hamza <hamzahalilovic.12@gmail.com>
2023-05-14 01:43:23 +0200

    navigation bar update

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,8 @@
 import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import {View, StyleSheet} from 'react-native';
+
 import ChatScreen from '../modules/ChatScreen';
 import ProfileScreen from '../modules/ProfileScreen';
 import SubscriptionsScreen from '../modules/SubscriptionsScreen';
@@ -16,6 +18,7 @@ import InfoLogo from '../assets/info-circle.svg';
 
 import AppContext from '../hoc/AppContext';
 import ChatScreenDemo from '../modules/ChatScreenDemo';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Tab = createBottomTabNavigator();
 
@@ -33,35 +36,84 @@ const AppNavigator = () => {
         name="About"
         component={AboutNavigator}
         options={{
-          tabBarIcon: ({focused, color, size}) => (
-            // <Icon name="info" size={size} color={color} />
-            <InfoLogo />
+          tabBarIcon: ({focused}) => (
+            <TouchableOpacity>
+              {focused ? (
+                <View style={styles.activeIcon}>
+                  <InfoLogo />
+                </View>
+              ) : (
+                <InfoLogo />
+              )}
+            </TouchableOpacity>
           ),
         }}
       />
       <Tab.Screen
         name="Chat"
         component={demo ? ChatScreenDemo : ChatScreen}
-        options={({navigation}) => ({
-          tabBarIcon: ({focused, color, size}) => <ChatLogo />,
-        })}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <TouchableOpacity>
+              {focused ? (
+                <View style={styles.activeIcon}>
+                  <ChatLogo />
+                </View>
+              ) : (
+                <ChatLogo />
+              )}
+            </TouchableOpacity>
+          ),
+        }}
       />
       <Tab.Screen
         name="Profile"
         component={ProfileScreen}
         options={{
-          tabBarIcon: ({focused, color, size}) => <UserLogo />,
+          tabBarIcon: ({focused}) => (
+            <TouchableOpacity>
+              {focused ? (
+                <View style={styles.activeIcon}>
+                  <UserLogo />
+                </View>
+              ) : (
+                <UserLogo />
+              )}
+            </TouchableOpacity>
+          ),
         }}
       />
       <Tab.Screen
         name="Subscriptions"
         component={SubscriptionsScreen}
         options={{
-          tabBarIcon: ({focused, color, size}) => <StarLogo />,
+          tabBarIcon: ({focused}) => (
+            <TouchableOpacity>
+              {focused ? (
+                <View style={styles.activeIcon}>
+                  <StarLogo />
+                </View>
+              ) : (
+                <StarLogo />
+              )}
+            </TouchableOpacity>
+          ),
         }}
       />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  activeIcon: {
+    alignContent: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    width: 36,
+    height: 36,
+    backgroundColor: '#F0FDF9',
+    borderRadius: 8,
+  },
+});
 
 export default AppNavigator;
