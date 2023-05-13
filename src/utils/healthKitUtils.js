9121ef984b1ce73196de6df328f6e2cfdb42cff4
hamza <hamzahalilovic.12@gmail.com>
2023-05-13 19:45:41 +0200

    hk

--- src/utils/healthKitUtils.js
+++ src/utils/healthKitUtils.js
@@ -13,37 +13,94 @@ const options = {
   endDate: new Date().toISOString(),
 };
 
-export const initHealthKit = async () => {
-  try {
-    await AppleHealthKit.initHealthKit(permissions);
-    Alert.alert(
-      'Success',
-      'HealthKit successfully initialized.',
-      [{text: 'OK', onPress: () => console.log('OK pressed')}],
-      {cancelable: false},
-    );
-  } catch (error) {
-    Alert.alert(
-      'Error',
-      'Failed to initialize HealthKit. Please try again.',
-      [{text: 'OK', onPress: () => console.log('OK pressed')}],
-      {cancelable: false},
-    );
-    throw error;
-  }
+import AppleHealthKit from 'react-native-health';
+import useDatabaseHooks from './useDatabaseHooks';
+
+const {insertData} = useDatabaseHooks();
+
+const initHealthKit = async () => {
+  const permissions = {
+    permissions: {
+      read: [AppleHealthKit.Constants.Permissions.StepCount],
+    },
+  };
+
+  AppleHealthKit.initHealthKit(permissions, error => {
+    if (error) {
+      console.log('[ERROR] Cannot grant permissions!');
+      return false;
+    }
+    return true;
+  });
 };
 
-export const getSamples = async () => {
-  try {
-    const results = await AppleHealthKit.getSamples(options);
-    return results;
-  } catch (error) {
-    Alert.alert(
-      'Error',
-      'Failed to retrieve health data. Please try again.',
-      [{text: 'OK', onPress: () => console.log('OK pressed')}],
-      {cancelable: false},
-    );
-    throw error;
-  }
+const getSteps = async () => {
+  const options = {
+    startDate: new Date(2023, 0, 1).toISOString(),
+    endDate: new Date().toISOString(),
+  };
+
+  AppleHealthKit.getSamples(options, (err, results) => {
+    if (err) {
+      console.log('error getting steps:', err);
+      return;
+    }
+    console.log('results', results);
+
+    // average steps
+    const stepsPerDay = {};
+
+    results.forEach(entry => {
+      const date = new Date(entry.start).toDateString(); // convert start time to date string - ignore hours  minutes  seconds
+      stepsPerDay[date] = (stepsPerDay[date] || 0) + entry.quantity; // accumulate steps per day
+    });
+
+    for (const [date, steps] of Object.entries(stepsPerDay)) {
+      insertData('Steps', 'steps', date, steps);
+    }
+  });
 };
+
+export {initHealthKit, getSteps};
+
+// export const handleHealthData = async (
+//   dataType,
+//   insertDataFunction,
+//   setIsLoading,
+//   setApiResponse,
+//   setError,
+//   setDataStatus,
+// ) => {
+//   setIsLoading(true);
+
+//   try {
+//     const results = await getSamples();
+
+//     const dataPerDay = {};
+
+//     results.forEach(entry => {
+//       const date = new Date(entry.start).toDateString();
+//       dataPerDay[date] = (dataPerDay[date] || 0) + entry.quantity;
+//     });
+
+//     for (const [date, data] of Object.entries(dataPerDay)) {
+//       insertDataFunction(date, data);
+//     }
+
+//     setIsLoading(false);
+//     setDataStatus(true);
+//   } catch (e) {
+//     console.log(e);
+//     setApiResponse('Something went wrong.');
+//     setError(e);
+//     setIsLoading(false);
+//     setDataStatus(false);
+
+//     Alert.alert(
+//       'Error',
+//       `Failed to retrieve health data. Please try again. ${error}`,
+//       [{text: 'OK', onPress: () => console.log('OK pressed')}],
+//       {cancelable: false},
+//     );
+//   }
+// };
